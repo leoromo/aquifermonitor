78f6d4fca3ace588aabd5c4176ece3c74db1181b
leoromo <leo.romo.m@gmail.com>
2017-09-24 01:22:57 -0500

    Use bluebird for mongoose promises

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var WaterRecord = require('./models/waterrecord');
+mongoose.Promise = require('bluebird');
 
 var app = express();
 var router = express.Router();
@@ -19,27 +20,42 @@ router.get('/', function(req, res) {
 });
 
 router.route('/records')
-	.get(function(req, res) {
-		WaterRecord.find(function(err, records) {
-			if (err)
-				res.send(err);
-			res.json(records);
-		});
-	})
-	.post(function(req, res) {
-		var record = new WaterRecord();
-		record.name = req.body.name;
-		record.location.lat = req.body.location.lat;
-		record.location.lon = req.body.location.lon;
-		record.user = req.body.user;
-		record.date = req.body.date;
-		record.data = req.body.data;
-		record.save(function(err) {
-			if (err)
-				res.send(err);
-			res.json({message: 'OK'});
-		});
+.get(function(req, res) {
+	WaterRecord.find(function(err, records) {
+		if (err)
+			res.send(err);
+		res.json(records);
 	});
+})
+.post(function(req, res) {
+	var record = new WaterRecord();
+	record.name = req.body.name;
+	record.location = req.body.location;
+	record.user = req.body.user;
+	record.date = req.body.date;
+	record.data = req.body.data;
+	record.save(function(err) {
+		if (err)
+			res.send(err);
+		res.json({message: 'OK'});
+	});
+});
+
+router.route('/records/:id')
+.get(function(req, res) {
+	WaterRecord.findById(req.params.id, function (err, record) {
+		if (err)
+			res.send(err);
+		res.json(record);
+	});
+})
+.delete(function(req, res) {
+	WaterRecord.remove({_id:req.params.id}, function(err, record) {
+		if (err)
+			res.send(err);
+		res.json('Deleted');
+	});
+});
 
 app.use('/api', router);
 
